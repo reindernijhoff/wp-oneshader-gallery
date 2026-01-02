3e1c65111c5c152b66e534196d119919d0ac78a4
reindernijhoff <reinder@infi.nl>
2026-01-02 21:51:37 +0100

    fixes

--- oneshader/oneshader-plugin.php
+++ oneshader/oneshader-plugin.php
@@ -66,7 +66,7 @@ function oneshader_list( $atts ) {
 	$list    = oneshader_do_query( $a['query'] );
 	$results = isset( $list['objects'] ) ? $list['objects'] : array();
 
-	$html = '<ul class="wp-block-gallery columns-' . esc_attr( $a['columns'] ) . ' is-cropped">';
+	$html = '<figure class="oneshader-gallery oneshader-columns-' . esc_attr( $a['columns'] ) . '">';
 
 	$start  = microtime( true );
 	$count  = 0;
@@ -86,7 +86,7 @@ function oneshader_list( $atts ) {
 		}
 	}
 
-	$html .= '</ul>';
+	$html .= '</figure>';
 	$html .= '<script type="application/ld+json">' . wp_json_encode( $ldjson ) . '</script>';
 
 	return $html;
@@ -132,20 +132,30 @@ function oneshader_ld_json( $info ) {
 // phpcs:disable
 // Images are served directly from oneshader.net so we always show the most up-to-date previews.
 function oneshader_layout_shader( $info, $hide_username ) {
-	$html  = '<li class="blocks-gallery-item"><figure>';
+	$html  = '<figure class="oneshader-gallery-item">';
 	$html .= '<a href="' . esc_url( $info['url'] ) . '" title="' . esc_attr( $info['title'] . ' by ' . $info['user_id'] ) . '">';
 	$html .= '<picture>';
 	$html .= '<source type="image/webp" srcset="' . esc_url( $info['webp'] ) . '" />';
 	$html .= '<img src="' . esc_url( $info['img'] ) . '" alt="' . esc_attr( str_replace( "\n", '&#10;', $info['description'] ) ) . '" width="512" height="512" />';
 	$html .= '</picture>';
-	$html .= '<figcaption>' . esc_html( $info['title'] ) . ( ! $hide_username ? '<br/>by ' . esc_html( $info['user_id'] ) : '' ) . '</figcaption>';
 	$html .= '</a>';
-	$html .= '</figure></li>';
+	$html .= '<figcaption>' . esc_html( $info['title'] ) . ( ! $hide_username ? '<br/>by ' . esc_html( $info['user_id'] ) : '' ) . '</figcaption>';
+	$html .= '</figure>';
 
 	return $html;
 }
 // phpcs:enable
 
+function oneshader_enqueue_styles() {
+	wp_enqueue_style(
+		'oneshader-gallery',
+		plugin_dir_url( __FILE__ ) . 'oneshader-gallery.css',
+		array(),
+		'1.0.0'
+	);
+}
+
 register_activation_hook( __FILE__, 'oneshader_install' );
+add_action( 'wp_enqueue_scripts', 'oneshader_enqueue_styles' );
 add_shortcode( 'oneshader-list', 'oneshader_list' );
 
